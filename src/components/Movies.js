089e28efbbc4ce55a7d91903052c782d7256453e
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-10-06 15:59:14 +0100

    jo

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import { movies } from '../data';
+import React from 'react'
+import { movies } from '../data'
 
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(movie => (
+        <div key={movie.title}>
+          <h1>{movie.title}</h1>
+          <h3>Time: {movie.time}</h3>
+          <h3>Time: {movie.metascore}</h3>
+          <ul>
+            {movie.genres.map(genre => (
+              <li>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      )
+      )}
     </div>
-  );
+  )
 };
 
-export default Movies;
+export default Movies
