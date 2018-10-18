ca50df4d03299b71a54456ab7df54cc0eb7b6eba
ritz-w <ritz.wu@outlook.com>
2018-10-18 12:33:37 +0100

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,15 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+        <NavLink to="/" exact>Home</NavLink>
+         <NavLink
+      to="/movies"
+      /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+      exact
+    >Movies</NavLink>
+    <NavLink to="/directors" exact>Directors</NavLink>
+    <NavLink to="/actors" exact>Actors</NavLink>
     </div>
   );
 };
