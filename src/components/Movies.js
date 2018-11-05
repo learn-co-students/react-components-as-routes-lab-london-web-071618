7de07dd42d7a23cd56d54b0d8419752af91c233c
Ali Aziz <aliaziz@Alis-MacBook-Pro.local>
2018-11-05 11:38:40 +0000

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import { movies } from '../data';
+import React from 'react'
+import { movies } from '../data'
 
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(movie => {
+        return <div>
+          <h2>{movie.title}</h2>
+          <p>Time: {movie.time}</p>
+          <ul>{movie.genres.map(genre => <li>{genre}</li>)}</ul>
+        </div>
+      })}
     </div>
-  );
-};
+  )
+}
 
-export default Movies;
+export default Movies
